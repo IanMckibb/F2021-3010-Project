7593d0128896a732bec85349a024c91262896f1e
WillM5462 <wimc5462@colorado.edu>
2021-10-10 18:36:40 -0600

    commiting test folder with test file

---
+++ F2021-3010-Project/Assets/test/testscript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Test script added in new folder to test version control
+
+public class testscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
