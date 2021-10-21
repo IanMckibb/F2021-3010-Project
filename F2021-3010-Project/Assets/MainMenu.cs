052163255ad91d66ff126a055d575b377ad1904f
WillM5462 <wimc5462@colorado.edu>
2021-10-21 14:32:40 -0600

    Added simple functioning UI to traverse game and menus

---
+++ F2021-3010-Project/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Load game scene
+    public void StartGame (){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Quit game
+    public void ExitGame (){
+        //quit does not work in unity editor so use debug to check
+        Debug.Log("Quitting Game.");
+        Application.Quit();
+    }
+}
