7db8dcc9c58e125224c412573778eae9648caf08
Meeperbunny <mckibbenian@gmail.com>
2021-12-09 15:27:01 -0700

    Enemey behavior updated

--- F2021-3010-Project/Assets/Scripts/Enemy.cs
+++ F2021-3010-Project/Assets/Scripts/Enemy.cs
@@ -6,48 +6,33 @@ public class Enemy : Entity
 {
     public float hp = 100.0f;
 
-    public float angle;
-    public float angleChange = 0.0f;
-    private float angleChangeRange = 0.05f;
-
     // public float speed; Speed already defined in Entity class
-    public float speedChange = 0.0f;
-    private float speedChangeRange = 1f;
 
-    private int fcount = 0;
+    private int frameCount = 0;
 
     // Update is called once per frame
 
     public override void Start()
     {
         this.addComponents();
-
-        this.angle = Random.Range(0, 2 * Mathf.PI);
-        this.speed = Random.Range(1, 2);
     }
     
     public override void Update()
     {
-        angleChange += Random.Range(-angleChangeRange, angleChangeRange) * Time.deltaTime;
-        angle += angleChange;
-
-        speedChange = Random.Range(-speedChangeRange, speedChangeRange) * Time.deltaTime;
-        speed += speedChange;
-
-        Move(new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)));
-
         //run check distance every 10 frames
-        fcount++;
-        if(fcount == 10){
+        frameCount++;
+        if(frameCount == 10){
             checkDistance();
-            fcount = 0;
+            frameCount = 0;
         }
     }
 
     public override void OnCollisionEnter2D(Collision2D col)
     {
-        hp -= 10.0f;
-        //print("Enemy lost hp, " + hp.ToString() + " left");
+        if (col.gameObject.name == "Player") {
+            hp -= 10.0f;
+        }
+        print("Enemy lost hp, " + hp.ToString() + " left");
         if (hp <= 0.0f) {
             DestroyEntity();
         }
